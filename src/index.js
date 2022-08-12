69df89276e971f462cf829bcbb11f51edcf6a019
Evgeniy Batenev <73171575+EugeneBatenev@users.noreply.github.com>
2022-08-13 02:53:32 +0300

    int: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+module.exports = function towelSort(matrix) {
 
-// You should implement your task here.
+    const newArr = [];
+    let result = [];
+    if (arguments.length === 0) {
+        return result;
+    } else if (matrix.length === 0) {
+        return result;
+    } else {
+        for (let i = 0; i < matrix.length; i++) {
+            if (i % 2 === 0) {
+                let string = matrix[i];
+                newArr.push(string);
+            } else if (i % 2 !== 0) {
+                let reverse = matrix[i].reverse();
+                newArr.push(reverse);
+            }
+        }
+    }
 
-module.exports = function towelSort (matrix) {
-  return [];
+    return result.concat(...newArr);
 }
